6dcd1dab9dbb00d888aa471428f1aef807d9bdd5
Akhilbabu456 <146112110+Akhilbabu456@users.noreply.github.com>
2024-01-11 23:42:06 +0530

    Delete Javascript/filterElmentsFromAnArray.js

--- Javascript/filterElmentsFromAnArray.js
+++
@@ -1,9 +0,0 @@
-var filter = function(arr, fn) {
-    let array=[]
-    for(let i=0;i<arr.length;i++){
-        if(fn(arr[i],i)){
-            array.push(arr[i])
-        }
-    }
-    return array
-};
